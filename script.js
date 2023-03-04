be1f4b0ddd341dd39e66fcfd7412ebb56b383ed9
surajyadav471 <115660802+surajyadav471@users.noreply.github.com>
2023-03-04 14:15:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
 function minCostToFormRope(a) {
-  a.preventDefault();
-  var inputElement = document.querySelector("input").value;
-  var arr = inputElement.split(",");
-  arr.sort(function (a, b) {
-    return a - b;
-  });
-
-  var cost = 0;
-  while (arr.length > 1) {
-    var res = Number(arr[0]) + Number(arr[1]);
-    arr.splice(0, 2);
-
-    arr.push(res);
-    cost += res;
-
-    arr.sort(function (a, b) {
-      return a - b;
-    });
-  }
-  document.getElementById("result").textContent = cost;
+	a.preventDefault();
+	console.log(a);
+	var inputElement = document.querySelector('input').value;
+	var arr = inputElement.split(',');
+	arr.sort(function (a,b) { return a-b});
+	var cost = 0;
+	while(arr.length > 1) {
+		var res = Number(arr[0]) + Number(arr[1]);
+		arr.splice(0, 2);
+		arr.push(res);
+		cost += res;
+		arr.sort(function (a, b) { return a-b});
+	}
+	document.getElementById("result").textContent = cost;
+	
 }
